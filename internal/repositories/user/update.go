ed5da3301f4626d3acf0c9152154260c501fd7ba
reactguy <nikolaj.botalov@gmail.com>
2025-04-22 21:32:26 +0500

    fix: update user pg request

--- internal/repositories/user/update.go
+++ internal/repositories/user/update.go
@@ -13,18 +13,26 @@ func (r *userRepository) Update(ctx context.Context, id string, user *domain.Use
 
 	user.UpdatedAt = time.Now()
 
-	query, args, err := sq.Update("users").
-		Set("first_name", user.FirstName).
-		Set("last_name", user.LastName).
-		Set("birthday", user.Birthday).
-		Set("updated_at", user.UpdatedAt).
-		Where(sq.Eq{"id": id}).PlaceholderFormat(sq.Dollar).ToSql()
+	updateBuilder := sq.Update("users")
+
+	if user.FirstName != nil {
+		updateBuilder = updateBuilder.Set("first_name", user.FirstName)
+	}
+	if user.LastName != nil {
+		updateBuilder = updateBuilder.Set("last_name", user.LastName)
+	}
+	if user.Birthday != nil {
+		updateBuilder = updateBuilder.Set("birthday", user.Birthday)
+	}
+	updateBuilder = updateBuilder.Set("updated_at", user.UpdatedAt)
+
+	query, args, err := updateBuilder.Where(sq.Eq{"id": id}).PlaceholderFormat(sq.Dollar).ToSql()
 	if err != nil {
 		r.logger.Error("Failed to build update user query", zap.String("id", id))
 		return err
 	}
 
-	result, err := r.db.Exec(ctx, query, args)
+	result, err := r.db.Exec(ctx, query, args...)
 	if err != nil {
 		r.logger.Error("Failed to update user", zap.String("id", id))
 		return err
